1153bc657e8b020ea3e8c271a6f2243af871c055
Joakim Larsson <joakimlarsson92@gmail.com>
2016-01-23 13:17:56 +0100

    Sort of fixed d3js

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('dh2321InformationVisualization', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+    .module('dh2321InformationVisualization', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr', 'd3']);
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,6 +13,13 @@
     vm.classAnimation = '';
     vm.creationDate = 1453539129321;
     vm.showToastr = showToastr;
+    vm.d3Data = [
+      {name: "Greg", score: 98},
+      {name: "Ari", score: 96},
+      {name: 'Q', score: 75},
+      {name: "Loser", score: 48}
+    ];
+
 
     activate();
 

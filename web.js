74d84af7195be7f10e405a5715a2d16c76890458
Joakim Larsson <joakimlarsson92@gmail.com>
2016-02-11 18:28:55 +0100

    Preparing for heroku

---
+++ web.js
@@ -0,0 +1,10 @@
+/**
+ * Created by farst_000 on 2016-02-11.
+ */
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);

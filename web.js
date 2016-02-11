e99d928066428cf2384348bd8ac740f081f4574a
Joakim Larsson <joakimlarsson92@gmail.com>
2016-02-11 18:37:31 +0100

    updating for heroku

--- web.js
+++ web.js
@@ -1,10 +1,8 @@
-/**
- * Created by farst_000 on 2016-02-11.
- */
 var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
-app.use(express.logger('dev'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);

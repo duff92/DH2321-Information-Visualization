c142ec719a02ac71621a4c4034fb6289ec7a29d4
Joakim Larsson <joakimlarsson92@gmail.com>
2016-03-01 00:56:22 +0100

    Working steam graph with brushing! :D

--- client/app/steamGraph/steamGraph.directive.js
+++ client/app/steamGraph/steamGraph.directive.js
@@ -10,32 +10,32 @@ angular.module('dh2321InformationVisualizationApp')
       link: function (scope, elem) {
         d3Service.d3().then(function(d3){
 
-          var format = d3.time.format("%m/%d/%Y");
+          var format = d3.time.format('%m/%d/%Y');
 
-          var margin = {top: 20, right:100, bottom: 30, left: 100};
-          var width = document.body.clientWidth - margin.left - margin.right;
-          var height = 400 - margin.top - margin.bottom;
+          var margin = {top: 10, right:100, bottom: 120, left: 50},
+              margin2 = {top: 410, right: 100, bottom: 30, left: 50},
+              width = document.body.clientWidth - margin.left - margin.right,
+              height = 500 - margin.top - margin.bottom,
+              height2 = 500 - margin2.top - margin2.bottom;
 
-          var colorrange = ["#045A8D",  "#74A9CF", "#D0D1E6"];
+          var colorrange = ['#22315c',  '#84e2e5', '#c9c9c9'];
           var strokecolor = colorrange[0];
-          var tooltip = d3.select(elem[0])
-            .append("div")
-            .attr("class", "remove")
-            .style("position", "absolute")
-            .style("z-index", "20")
-            .style("visibility", "hidden")
-            .style("top", "100px")
-            .style("left", "150px");
-/*
-          var svg = d3.select(elem[0]).append('svg')
-            .attr('width', width)
-            .attr('height', height);*/
 
-          var svg = d3.select(elem[0]).append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          var svg = d3.select(elem[0]).append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+          var tooltip = d3.select(elem[0])
+            .append('div')
+            .attr('class', 'remove')
+            .style('position', 'absolute')
+            .style('z-index', '20')
+            .style('visibility', 'hidden')
+            .style('top', '200px')
+            .style('left', '150px');
 
           // on window resize, re-render d3 canvas
           window.onresize = function() {
@@ -58,28 +58,24 @@ angular.module('dh2321InformationVisualizationApp')
           scope.render = function(data){
             svg.selectAll('*').remove();
 
-            var x = d3.time.scale()
-              .range([0, width]);
+            //Dimensions
+            var x = d3.time.scale().range([0, width]),
+                x2 = d3.time.scale().range([0, width]),
+                y = d3.scale.linear().range([height, 0]),
+                y2 = d3.time.scale().range([height2, 0]),
+                z = d3.scale.ordinal().range(colorrange),
+                z2 = d3.scale.ordinal().range(colorrange);
 
-            var y = d3.scale.linear()
-              .range([height-10, 0]);
+            //Axis
+            var xAxis = d3.svg.axis().scale(x).orient('bottom'),
+                xAxis2 = d3.svg.axis().scale(x2).orient('bottom'),
+                yAxis = d3.svg.axis().scale(y);
 
-            var z = d3.scale.ordinal()
-              .range(colorrange);
+            var brush = d3.svg.brush().x(x2).on('brush', brushed);
 
-            var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("bottom")
-              .ticks(d3.time.month);
-
-            var yAxis = d3.svg.axis()
-              .scale(y);
-
-            var yAxisr = d3.svg.axis()
-              .scale(y);
 
             var stack = d3.layout.stack()
-              .offset("silhouette")
+              .offset('silhouette')
               .values(function(d) { return d.values; })
               .x(function(d) { return d.date; })
               .y(function(d) { return d.clicks; });
@@ -87,108 +83,196 @@ angular.module('dh2321InformationVisualizationApp')
             var nest = d3.nest().key(function(d){ return d.consumpType;});
 
             var area = d3.svg.area()
-              .interpolate("cardinal")
+              .interpolate('cardinal')
               .x(function(d) { return x(d.date); })
               .y0(function(d) { return y(d.y0); })
               .y1(function(d) { return y(d.y0 + d.y); });
 
+            var area2 = d3.svg.area()
+              .interpolate('cardinal')
+              .x(function(d) { return x2(d.date); })
+              .y0(function(d) { return y2(d.y0); })
+              .y1(function(d) { return y2(d.y0 + d.y); });
 
-            scope.data.forEach(function(d){
+            svg.append('defs').append('clipPath')
+              .attr('id', 'clip')
+              .append('rect')
+              .attr('width', width)
+              .attr('height', height);
+
+            var focus = svg.append('g')
+              .attr('class', 'focus')
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+            var context = svg.append('g')
+              .attr('class', 'context')
+              .attr('transform', 'translate(' + margin2.left + ',' + margin2.top + ')');
+
+            data.forEach(function(d){
              // console.log(d.date);
               if(typeof(d.date) !== 'object'){
                 d.date = format.parse(d.date);
                 d.clicks = +d.clicks;
               }
             });
-            var layers = stack(nest.entries(scope.data));
+            var layers = stack(nest.entries(data));
             console.log(layers);
+            x.domain(d3.extent(data, function(d) { return d.date; }));
+            y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
+            x2.domain(x.domain());
+            y2.domain(y.domain());
 
-            x.domain(d3.extent(scope.data, function(d) { return d.date; }));
-            y.domain([0, d3.max(scope.data, function(d) { return d.y0 + d.y; })]);
 
-            svg.selectAll(".layer")
+            focus.selectAll('.area')
               .data(layers)
-              .enter().append("path")
-              .attr("class", "layer")
-              .attr("d", function(d) { return area(d.values); })
-              .style("fill", function(d, i) { return z(i); });
+              .enter().append('path')
+              .attr('class', 'area')
+              .attr('d', function(d) {return area(d.values);})
+              .style('fill', function(d, i){return z(i);});
 
-            svg.append("g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + height + ")")
+            focus.append('g')
+              .attr('class', 'x axis')
+              .attr('transform', 'translate(0,' + height + ')')
               .call(xAxis);
 
-            svg.append("g")
-              .attr("class", "y axis")
-              .attr("transform", "translate(" + width + ", 0)")
-              .call(yAxis.orient("right"));
+            focus.append('g')
+              .attr('class', 'y axis')
+              .call(yAxis.orient('left'));
 
-            svg.append("g")
-              .attr("class", "y axis")
-              .call(yAxis.orient("left"));
-            var datearray = [];
-            svg.selectAll(".layer")
-              .attr("opacity", 1)
-              .on("mouseover", function(d, i) {
-                svg.selectAll(".layer").transition()
-                  .duration(250)
-                  .attr("opacity", function(d, j) {
-                    return j != i ? 0.6 : 1;
-                  })})
-              .on("mousemove", function(d, i) {
-                console.log(d);
-                var mousex = d3.mouse(this);
-                mousex = mousex[0];
-                var invertedx = x.invert(mousex);
-                invertedx = invertedx.getMonth() + invertedx.getDate();
-                var selected = (d.values);
-                for (var k = 0; k < selected.length; k++) {
-                  datearray[k] = selected[k].date;
-                  datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
-                 // console.log(selected[k].date.getMonth());
+            context.selectAll('.area')
+              .data(layers)
+              .enter().append('path')
+              .attr('class', 'area')
+              .attr('d', function(d) {return area2(d.values);})
+              .style('fill', function(d, i){return z2(i);});
+
+            context.append('g')
+              .attr('class', 'x axis')
+              .attr('transform', 'translate(0,' + height2 + ')')
+              .call(xAxis2);
+
+            context.append('g')
+              .attr('class', 'x brush')
+              .call(brush)
+              .selectAll('rect')
+              .attr('y', -6)
+              .attr('height', height2 + 7);
+
+            /*svg.selectAll('.layer')
+              .data(layers)
+              .enter().append('path')
+              .attr('class', 'layer')
+              .attr('d', function(d) { return area(d.values); })
+              .style('fill', function(d, i) { return z(i); });*/
+
+
+            /*svg.append('g')
+              .attr('class', 'x axis')
+              .attr('transform', 'translate(0,' + height + ')')
+              .call(xAxis);
+
+            svg.append('g')
+              .attr('class', 'y axis')
+              .call(yAxis.orient('left'));*/
+
+            function brushed() {
+              x.domain(brush.empty() ? x2.domain() : brush.extent());
+              var dataFiltered = data.filter(function(d) {
+                if ( (d.date >= x.domain()[0]) && (d.date <= x.domain()[1]) ) {
+                  return d;
                 }
-
-                var mousedate = datearray.indexOf(invertedx);
-                var pro = d.values[mousedate].clicks.toString();
-                d3.select(this)
-                  .classed("hover", true)
-                  .attr("stroke", strokecolor)
-                  .attr("stroke-width", "1px");
-                  tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
-
-              })
-              .on("mouseout", function(d, i) {
-                svg.selectAll(".layer")
-                  .transition()
-                  .duration(250)
-                  .attr("opacity", "1");
-                d3.select(this)
-                  .classed("hover", false)
-                  .attr("stroke-width", "0px");
-                tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
               });
+              dataFiltered.forEach(function(d){
+                // console.log(d.date);
+                if(typeof(d.date) !== 'object'){
+                  d.date = format.parse(d.date);
+                  d.clicks = +d.clicks;
+                }
+              });
+              var layers2 = stack(nest.entries(dataFiltered));
 
+              console.log(dataFiltered);
+              x.domain(d3.extent(dataFiltered, function(d) { return d.date; }));
+              y.domain([0, d3.max(dataFiltered, function(d) { return d.y0 + d.y; })]);
+
+              focus.selectAll('.area').remove();
+              focus.selectAll('.area')
+                .data(layers2)
+                .enter().append('path')
+                .attr('class', 'area')
+                .attr('d', function(d) {return area(d.values);})
+                .style('fill', function(d, i){return z(i);});
+
+              focus.select('.x.axis').call(xAxis);
+              focus.select('.y.axis').call(yAxis.orient('left'));
+
+              var datearray = [];
+              var pro;
+              focus.selectAll('.area')
+                .attr('opacity', 1)
+                .on('mouseover', function(d, i) {
+                  focus.selectAll('.area').transition()
+                    .duration(250)
+                    .attr('opacity', function(d, j) {
+                      return j != i ? 0.6 : 1;
+                    })})
+                .on('mousemove', function(d, i) {
+                  console.log(d);
+                  var mousex = d3.mouse(this);
+                  mousex = mousex[0];
+                  var invertedx = x.invert(mousex);
+                  invertedx = invertedx.getMonth() + invertedx.getDate();
+                  var selected = (d.values);
+                  for (var k = 0; k < selected.length; k++) {
+                    datearray[k] = selected[k].date;
+                    datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
+                    // console.log(selected[k].date.getMonth());
+                  }
+
+                  var mousedate = datearray.indexOf(invertedx);
+                  pro = d.values[mousedate].clicks.toString();
+
+                  d3.select(this)
+                    .classed('hover', true)
+                    .attr('stroke', strokecolor)
+                    .attr('stroke-width', '1px');
+                  tooltip.html( '<p class="lead">' + d.key + '<br>' + pro + '</p>' ).style('visibility', 'visible');
+
+                })
+                .on('mouseout', function(d, i) {
+                  focus.selectAll('.area')
+                    .transition()
+                    .duration(250)
+                    .attr('opacity', '1');
+                  d3.select(this)
+                    .classed('hover', false)
+                    .attr('stroke-width', '0px');
+                  tooltip.html( '<p>' + d.key + '<br>' + pro + '</p>' ).style('visibility', 'hidden');
+                });
+
+            }
+           /* d3.select('.remove').remove();
             var vertical = d3.select(elem[0])
-              .append("div")
-              .attr("class", "remove")
-              .style("position", "absolute")
-              .style("z-index", "19")
-              .style("width", "1px")
-              .style("height", "400px")
-              .style("top", "50px")
-              .style("bottom", "30px")
-              .style("left", "0px")
-              .style("background", "#000");
+              .append('div')
+              .attr('class', 'remove')
+              .style('position', 'absolute')
+              .style('z-index', '19')
+              .style('width', '1px')
+              .style('height', '380px')
+              .style('top', '185px')
+              .style('bottom', '0px')
+              .style('left', '100px')
+              .style('background', '#000');
 
             d3.select(elem[0])
-              .on("mousemove", function(){
+              .on('mousemove', function(){
                 var mousex = d3.mouse(this);
                 mousex = mousex[0] + 5;
-                vertical.attr('fill', 'black').style("left", mousex + "px" )})
-              .on("mouseover", function(){
+                vertical.attr('fill', 'black').style('left', mousex + 'px' )})
+              .on('mouseover', function(){
                 var mousex = d3.mouse(this);
                 mousex = mousex[0] + 5;
-                vertical.attr('fill', 'black').style("left", mousex + "px")});
+                vertical.attr('fill', 'black').style('left', mousex + 'px')});*/
 
            /* var dataToUse = [];
             var minDate = new Date();
@@ -227,66 +311,10 @@ angular.module('dh2321InformationVisualizationApp')
                   //console.log(d);
                   return d.y0 + d.y;
                 });
-            });
-
-
-            var x = d3.scale.linear()
-              .domain([0, m - 1])
-              .range([0, width]);
-
-
-
-            var y = d3.scale.linear()
-              .domain([0, yDomain])
-              .range([height, 0]);
-
-            //Color scale (linear interpolation) of steam graph
-            var color = d3.scale.linear()
-              .range(['#aad', '#556']);
-
-            var area = d3.svg.area()
-              .x(
-                function(d) {
-                  return x(d.x);
-                })
-              .y0(function(d) {
-                return y(d.y0);
-              })
-              .y1(function(d) {
-                return y(d.y0 + d.y);
-              });
-
-            svg.selectAll('path')
-              .data(layers0)
-              .enter().append('path')
-              .attr('d', function(d) { return area(d.values); })
-              .style('fill', function() { return color(Math.random()); });
-            var xAxis = d3.svg.axis()
-              .scale(d3.time.scale().range([minDate,maxDate]).domain([0,width]))
-              .orient("bottom")
-              .ticks(d3.time.year);
-            svg.append("g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + height + ")")
-              .call(xAxis);*/
+            });*/
           };
 
 
-
-
-
-
-          /*function transition() {
-            d3.selectAll('path')
-              .data(function() {
-                var d = layers1;
-                layers1 = layers0;
-                return layers0 = d;
-              })
-              .transition()
-              .duration(2500)
-              .attr('d', area);
-          }*/
         });
       }
     };
